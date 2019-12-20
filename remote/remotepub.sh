de1d3ae4681084b42375b754a7eee1536d5af1ee
jihuayi <jihuayi@corp.netease.com>
2019-12-20 19:47:44 +0800

    add remotepub.sh

---
+++ remote/remotepub.sh
@@ -0,0 +1,27 @@
+project=$1
+branch=$2
+version=$3
+
+case $project in
+    ezlib)
+        #ezlib update
+        #dependence update
+        #post
+    ;;
+
+    sdk)
+        #sdk branch
+        #ezlib update
+        #dependence update
+        #sdk update
+        #post
+    ;;
+
+    stub)
+        #stub branch
+        #ezlib update
+        #dependence update
+        #stub update
+        #post
+    ;;
+esac
